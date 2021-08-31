70587053949a4ad9f064c55a23f2973f2c7ca006
ChuhanXU <chx37@pitt.edu>
2021-08-31 11:30:02 -0400

    add upload dataset function

--- application/routes.py
+++ application/routes.py
@@ -4,54 +4,68 @@ import pandas as pd
 import json
 import plotly
 import plotly.express as px
-
+from werkzeug.utils import secure_filename
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('scatter.html')
 
 @app.route('/line')
 def line():
-    return render_template('line.html')
+    return render_template('trend.html')
 @app.route('/scatter')
 def scatter():
-    return render_template('index.html')
+    return render_template('scatter.html')
 @app.route('/threeD')
 def threeD():
     return render_template('threeD.html')
 
-@app.route('/chart1', methods=['POST'])
+@app.route('/chart1', methods=['POST','GET'])
 def chart1():
     if request.method == "POST":
         x_label = request.form.get("x_label", None)
         y_label = request.form.get("y_label", None)
-        dataset_name = request.form.get("dataset_name", None)
-        print(x_label)
-        print(y_label)
-        print(dataset_name)
+        if request.files:
+            print(request.files)
+            f = request.files['dataset']
+            print(str(secure_filename(f.filename)))
+            upload_path = "dataset/retrieve/" + str(secure_filename(f.filename))
+            f.save(upload_path)
+            dataset_name = str(secure_filename(f.filename))
+        else:
+            dataset_name = request.form.get("dataset_name", None) + ".csv"
+
 
     # Graph One
         if x_label and y_label and dataset_name:
-            df = pd.read_csv("dataset/retrieve/"+str(dataset_name)+".csv")
+            df = pd.read_csv("dataset/retrieve/"+str(dataset_name))
             title = "Dataset Name:" + str(dataset_name)
             fig1 = px.scatter(x= df[x_label], y=df[y_label], color = df[y_label],title= title,labels=dict(x=x_label, y=y_label))
             graph1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
-    return render_template('index.html', graph1JSON=graph1JSON)
+    return render_template('scatter.html', graph1JSON=graph1JSON)
 
 @app.route('/chart2', methods=['POST'])
 def chart2():
         if request.method == "POST":
             x_label = request.form.get("x_label", None)
             y_label = request.form.get("y_label", None)
-            dataset_name = request.form.get("dataset_name", None)
+            if request.files:
+                print(request.files)
+                f = request.files['dataset']
+                print(str(secure_filename(f.filename)))
+                upload_path = "dataset/retrieve/" + str(secure_filename(f.filename))
+                f.save(upload_path)
+                dataset_name = str(secure_filename(f.filename))
+            else:
+                dataset_name = request.form.get("dataset_name", None) + ".csv"
             # Graph Two
             if x_label and y_label and dataset_name:
-                df = pd.read_csv("dataset/retrieve/" + str(dataset_name) + ".csv")
+                df = pd.read_csv("dataset/retrieve/" + str(dataset_name))
                 title = "Dataset Name:" + str(dataset_name)
                 fig2 = px.line(x=df[x_label], y=df[y_label], color=df[y_label],title=title, labels=dict(x=x_label, y=y_label))
                 graph2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
-        return render_template('line.html', graph2JSON=graph2JSON)
+        return render_template('trend.html', graph2JSON=graph2JSON)
 
 @app.route('/chart3', methods=['POST'])
 def chart3():
@@ -59,14 +73,31 @@ def chart3():
             x_label = request.form.get("x_label", None)
             y_label = request.form.get("y_label", None)
             z_label = request.form.get("z_label", None)
-            dataset_name = request.form.get("dataset_name", None)
-            # Graph Three
+            if request.files:
+                print(request.files)
+                f = request.files['dataset']
+                print(str(secure_filename(f.filename)))
+                upload_path = "dataset/retrieve/" + str(secure_filename(f.filename))
+                f.save(upload_path)
+                dataset_name = str(secure_filename(f.filename))
+            else:
+                dataset_name = request.form.get("dataset_name", None) + ".csv"
             if x_label and y_label and dataset_name:
-                df = pd.read_csv("dataset/retrieve/" + str(dataset_name) + ".csv")
+                df = pd.read_csv("dataset/retrieve/" + str(dataset_name))
                 title = "Dataset Name:" + str(dataset_name)
                 fig3 = px.scatter_3d(x=df[x_label], y=df[y_label], z=df[z_label],title=title,color=df[z_label], labels=dict(x=x_label, y=y_label, z=z_label))
                 graph3JSON = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
         return render_template('threeD.html', graph3JSON=graph3JSON)
+# @app.route('/upload', methods = ['POST','GET'])
+# def upload():
+#     if request.method == 'POST':
+#         f = request.files['file']
+#         print(str(secure_filename(f.filename)))
+#         upload_path = "dataset/retrieve/" + str(secure_filename(f.filename))
+#         f.save(upload_path)
+
+
+
 
 
     # Graph two
@@ -81,5 +112,5 @@ def chart3():
     # graph3JSON = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
 
 
-    #return render_template('index.html', graph1JSON=graph1JSON,  graph2JSON=graph2JSON, graph3JSON=graph3JSON)
-    #return render_template('index.html')
+    #return render_template('scatter.html', graph1JSON=graph1JSON,  graph2JSON=graph2JSON, graph3JSON=graph3JSON)
+    #return render_template('scatter.html')
