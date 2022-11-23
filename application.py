9030c4ba0c0f79f68f418fe6bfef30af42120b49
HakunaZoe <yiz209@pitt.edu>
2022-11-23 12:04:21 -0500

    upload learn_aws file

--- application.py
+++ application.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     application.secret_key = 'super secret key'
     application.config['SESSION_TYPE'] = 'filesystem'
     application.config['MAX_CONTENT_LENGTH'] = 1000 * 1024 * 1024
-    #application.run(debug=True)
-    application.run(host='0,0,0,0',port=5000)
+    application.run(debug=True)
+    #application.run(host='0,0,0,0',port=5000,debug=True)
